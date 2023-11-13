49d0d453bb48f6532fab8899c519c57fb6f9451a
psterner <psterner@hotmail.com>
2023-11-13 15:00:49 +0000

    Add buttons to increment or decrement counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
 
 export const App = () => {
   const counter = useSelector((state) => state.counter);
-
+  const isLoggedIn = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
   return (
     <>
       <h1>Counter: {counter} </h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+
+      {isLoggedIn ? <h3>Show message only to logged in members.</h3> : ""}
     </>
   );
 };
