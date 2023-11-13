1867fa0c8f2f00bb06d89608bea7e7121f0a07e0
psterner <psterner@hotmail.com>
2023-11-13 14:40:43 +0000

    Connect the store to App using Provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
 import "./index.css";
+import { Provider } from "react-redux";
 import { createStore } from "redux";
+import allReducer from "./reducers";
+
+const store = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
